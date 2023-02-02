29c9590b123dcc8124062f854f3b9b25f9218db2
eoox <siwall27@gmail.com>
2023-02-02 18:06:25 +0900

    animator 추가

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,6 +1,7 @@
 import 'package:angela_firebase/screens/login_screen.dart';
 import 'package:angela_firebase/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'WelcomeScreen';
@@ -8,30 +9,76 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  // late AnimationController controller;
+  // late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // controller =
+    //     AnimationController(duration: const Duration(seconds: 1), vsync: this);
+    // animation = CurvedAnimation(parent: controller, curve: Curves.easeInQuint);
+    // animation = ColorTween(begin: Colors.blueGrey, end: Colors.white).animage(controller);
+
+    // controller.forward();
+
+    // controller.addListener(() {
+    //   setState(() {});
+    // });
+  }
+
+  @override
+  void dispose() {
+    // contoller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.amber.withOpacity(controller.value),
       backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
+          children: [
             Row(
-              children: <Widget>[
-                SizedBox(
-                  height: 60.0,
-                  child: Image.asset('images/logo.png'),
-                ),
-                const Text(
-                  'Flash Chat',
-                  style: TextStyle(
-                    fontSize: 45.0,
-                    fontWeight: FontWeight.w900,
+              children: [
+                Hero(
+                  tag: 'App_logo',
+                  child: SizedBox(
+                    // height: animation.value * 100,
+                    height: 60.0,
+                    child: Image.asset('images/logo.png'),
                   ),
                 ),
+                SizedBox(
+                  width: 300,
+                  child: AnimatedTextKit(
+                    totalRepeatCount: 1,
+                    animatedTexts: [
+                      TypewriterAnimatedText('Flash Chat',
+                          speed: const Duration(milliseconds: 100),
+                          cursor: '|',
+                          textStyle: const TextStyle(
+                              fontSize: 45.0, fontWeight: FontWeight.w900)),
+                    ],
+                  ),
+                ),
+
+                // const Text(
+                //   // '${controller.value.toInt()}%',
+                //   'Flash Chat',
+                //   style: TextStyle(
+                //     fontSize: 45.0,
+                //     fontWeight: FontWeight.w900,
+                //   ),
+                // ),
               ],
             ),
             const SizedBox(
